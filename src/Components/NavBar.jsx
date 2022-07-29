b0bfd7d5d7749c35005a40a5b2ab4f4ae5879600
Darab <darabcode@gmail.com>
2022-07-29 23:45:36 +0100

    Added responsiveness to the page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./navbar.module.css";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
 
+const navbar = style.navbar_list;
+const close = style.close;
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNavToggle = () => {
+    setNav((pro) => !pro);
+  };
+
   return (
     <nav className={style.navbar_wrapper}>
       <h1>ToreHub</h1>
-      <ul className={style.navbar_list}>
+      <ul className={`${nav ? navbar : close}`}>
         <li>Home</li>
         <li>Service</li>
         <li>Project</li>
@@ -16,6 +26,10 @@ const NavBar = () => {
           <a href="/">Get Started</a>
         </li>
       </ul>
+
+      <div onClick={handleNavToggle} className={style.nav_icon}>
+        {nav ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </div>
     </nav>
   );
 };
